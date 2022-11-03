f3fb23159af47b48aec5c2c9273c11738cdf6a6f
RasmusBV <rasmus.b.v.pedersen@gmail.com>
2022-11-03 23:10:03 +0100

    Tilføjet Gennemsnitlig Forsinkelse

--- DelayPlotting.py
+++ DelayPlotting.py
@@ -6,11 +6,26 @@ def plotKumulativSandsynlighed(forsinkelser: "list[pd.Series]", grafTitler: "lis
     ax = fig.add_subplot(111)
     ax.set_xlabel('min')
     ax.set_ylabel('Kumulativ Sandsynlighed')
+    ax.set_title(titel)
     for i in range(len(forsinkelser)):
         delayList = forsinkelser[i].to_list()
         ax.hist(delayList, label=grafTitler[i], density=True, bins=150, range=(-50, 100), histtype='step', cumulative=True)
     ax.legend(bbox_to_anchor=(0.9,0.3))
+    if saveLocation == None:
+        plt.show()
+    else:
+        plt.savefig(f"{saveLocation}/{titel}")
+
+def plotGnsForsinkelse(forsinkelser: "list[pd.Series]", soejleTitler: "list[str]", titel: str, saveLocation = None):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_ylabel("min")
+    gnsForsinkelse = []
     ax.set_title(titel)
+    for i in range(len(forsinkelser)):
+        positiveForsinkelser = forsinkelser[i][forsinkelser[i] > 0]
+        gnsForsinkelse.append(positiveForsinkelser.sum()/forsinkelser[i].size)
+    ax.bar(soejleTitler, gnsForsinkelse)
     if saveLocation == None:
         plt.show()
     else:

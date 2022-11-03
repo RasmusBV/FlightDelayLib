6e68b726a60755a04aac6d556f2920461463dd14
RasmusBV <rasmus.b.v.pedersen@gmail.com>
2022-11-03 16:47:21 +0100

    Fixede plotKumulativSandsynlighed

--- DelayPlotting.py
+++ DelayPlotting.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import matplotlib.pylab as plt
 
-def plotKumulativSandsynlighed(forsinkelser: list[pd.Series], grafTitler: list[str], titel: str):
+def plotKumulativSandsynlighed(forsinkelser: "list[pd.Series]", grafTitler: "list[str]", titel: str):
     fig = plt.figure()
-    ax = fig.add_subfigure(111)
+    ax = fig.add_subplot(111)
     ax.set_xlabel('min')
     ax.set_ylabel('Kumulativ Sandsynlighed')
     for i in range(len(forsinkelser)):
@@ -12,5 +12,3 @@ def plotKumulativSandsynlighed(forsinkelser: list[pd.Series], grafTitler: list[s
     ax.legend(bbox_to_anchor=(0.9,0.3))
     ax.set_title(titel)
     plt.show()
-
-
